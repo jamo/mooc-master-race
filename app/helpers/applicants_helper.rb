d87d7f085e372b774cec9f492ccf74b0e74dfa9a
Jarmo Isotalo <jamo@isotalo.fi>
2014-04-17 22:52:11 +0300

    nicer view

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -1,2 +1,9 @@
 module ApplicantsHelper
+  def format_week_percents(percent)
+    if percent >= 85
+      raw "<span class=\"completed\">#{number_to_human(percent)}%</span>"
+    else
+      raw "<span class=\"not-yet\">#{number_to_human(percent)}%</span>"
+    end
+  end
 end
