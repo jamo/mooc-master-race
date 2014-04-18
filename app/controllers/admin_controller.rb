40b77cdf13f8598b9ce8a895d2fa9011756b6ef4
Jarmo Isotalo <jamo@isotalo.fi>
2014-04-19 00:21:57 +0300

    admin view and default settings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,8 @@
 class AdminController < ApplicationController
-  def idex
+  def index
+    return access_denied unless admin?
+
+    Settings.email_template = params[:email_template] if params[:email_template]
+    Settings.questionary_notes = params[:questionary_notes] if params[:questionary_notes]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'admin/idex'
-
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :sessions
   resources :applicants
   resources :interviews
+  resources :admin
 
 
   root to: 'applicants#index'
