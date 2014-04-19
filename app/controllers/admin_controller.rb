1ff8b1a21d34ae2c5e07682653b26e5908b5f0da
Jarmo Isotalo <jamo@isotalo.fi>
2014-04-19 03:03:59 +0300

    layout fixes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,10 @@ class AdminController < ApplicationController
   def index
     return access_denied unless admin?
 
+    if params[:refresh_from_tmc]
+      Applicant.update_all_data_with_tmc
+      redirect_to admin_index_path, notice: "Updated data"
+    end
     Settings.email_template = params[:email_template] if params[:email_template]
     Settings.questionary_notes = params[:questionary_notes] if params[:questionary_notes]
   end
