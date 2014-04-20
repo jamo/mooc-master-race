9fd7155fa98a11534dcb04e1ec5b4da72e03b5bb
Jarmo Isotalo <jamo@isotalo.fi>
2014-04-20 18:12:04 +0300

    day generation and view

--- app/models/interview_day.rb
+++ app/models/interview_day.rb
@@ -7,19 +7,21 @@ class InterviewDay < ActiveRecord::Base
 
     def init_db
       interview_days = (DateTime.new(2014,4,25)..DateTime.new(2014,5,16)).map do |day|
-        #next if is_weekday?(day)
+        next unless is_weekday?(day)
         day = day.change({hour: 8 })
+        puts
+        puts day.strftime("%a %d.%m.%Y klo  %H:%M")
         InterviewDay.create!(date: day)
       end
-
-      interview_days.map do |interview_day|
+      interview_days.each do |interview_day|
+        next if interview_day.nil?
         acc_time = interview_day.date
         interview_day.interviews << Interview.create!(start_time: acc_time, duration_in_minutes: 20)
         end_time = interview_day.date.dup.change({hour: 15,min: 30})
         loop do
           acc_time += 15.minutes
-          p ACC: acc_time
-          break if end_time <= acc_time
+          #p ACC: acc_time
+          break if end_time < acc_time
           interview_day.interviews << Interview.create!(start_time: acc_time, duration_in_minutes: 20)
         end
       end
