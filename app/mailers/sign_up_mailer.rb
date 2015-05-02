01e13d75455657a0e5dacf846e64c73c18095df5
Jarmo Isotalo <jamo@isotalo.fi>
2015-05-03 02:29:06 +0300

    More forgotten files

---
+++ app/mailers/sign_up_mailer.rb
@@ -0,0 +1,14 @@
+class SignUpMailer < ActionMailer::Base
+  default from: "mooc@cs.helsinki.fi"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.sign_up_mailer.sign_up.subject
+  #
+  def sign_up(to, title, message)
+
+    @message = message
+    mail to: to, subject: title, bcc: "jamo+mooc-master-sent@jamo.io,mooc@cs.helsinki.fi"
+  end
+end
